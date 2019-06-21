76f9c7352cf627c652a16da992e8dbe43c5b616a
Rodrigo <rodrigo.demetrio.palma@gmail.com>
2019-06-21 18:08:47 -0300

    fix detail

--- src/controllers/EquityController.js
+++ src/controllers/EquityController.js
@@ -8,7 +8,7 @@ module.exports = {
     async index(req, res) {
     request('https://www.alphavantage.co/query?function='+req.params.function+'&symbol='+req.params.symbol+'&apikey=XJC03LW4NGCASJIO', function (error, response, body) {
     if (!error && response.statusCode == 200) {
-        console.log(body) // Show the HTML for the Google homepage. 
+        //console.log(body) // Show the HTML for the Google homepage. 
         return res.send(body);
     }
     });
