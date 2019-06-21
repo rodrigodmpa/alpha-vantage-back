c1037278432d67d7fec8e2becd13de4333dd850f
Rodrigo <rodrigo.demetrio.palma@gmail.com>
2019-06-21 19:24:42 -0300

    security issues fixed

--- src/controllers/EquityController.js
+++ src/controllers/EquityController.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 var request = require('request');
 
 const Equity = mongoose.model('Equity');
-
+api_key = process.env.ALPHA_VANTAGE_KEY
 
 module.exports = {
     async index(req, res) {
-    request('https://www.alphavantage.co/query?function='+req.params.function+'&symbol='+req.params.symbol+'&apikey=XJC03LW4NGCASJIO', function (error, response, body) {
+    request('https://www.alphavantage.co/query?function='+req.params.function+'&symbol='+req.params.symbol+'&apikey='+ api_key, function (error, response, body) {
     if (!error && response.statusCode == 200) {
         //console.log(body) // Show the HTML for the Google homepage. 
         return res.send(body);
